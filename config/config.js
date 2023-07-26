8c8fadf246b404d7916707cefb59b7e3ed8099ae
hcl-z <60871037+hcl-z@users.noreply.github.com>
2023-07-26 02:55:22 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ config/config.js
@@ -0,0 +1,34 @@
+const config = {
+  mode: 'site',
+  title: 'JavaScript Guidebook',
+  description: 'JavaScript 完全知识体系',
+  base: '/javascript-guidebook/',
+  publicPath: '/javascript-guidebook/',
+  favicon: './favicon.ico',
+  logo: 'http://img.mrsingsing.com/javascript-guidebook-favicon.png',
+  hash: true,
+  exportStatic: {},
+  navs: [
+    null,
+    {
+      title: 'Github',
+      path: 'https://github.com/tsejx/javascript-guidebook',
+    },
+  ],
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css',
+      },
+    ],
+  ],
+};
+
+if (process.env.NODE_ENV !== 'development') {
+  config.ssr = {};
+}
+
+export default config;
